2ad5e9f8f5088154e1982664ba8f9db2c2461d7f
Brb House on Fire <bobbywoj@sbcglobal.net>
2019-12-15 19:31:52 -0600

    initial layout build

---
+++ server.js
@@ -0,0 +1,37 @@
+// Dependencies
+const express = require("express");
+const mysql = require("mysql");
+// Create instance of express app.
+let app = express();
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "123asdjkl",
+    database: "employee_db"
+  });
+  // Initiate MySQL Connection.
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
+
+
+// connection.query("SELECT * FROM actors WHERE attitude = ?", [req.params.att], function(err, result) {
+//     if (err) throw err;
+//     var html = "<h1>Actors With an Attitude of " + req.params.att + "</h1>";
+//     html += "<ul>";
+//     for (var i = 0; i < result.length; i++) {
+//       html += "<li><p> ID: " + result[i].id + "</p>";
+//       html += "<p> Name: " + result[i].name + "</p>";
+//       html += "<p> Coolness Points: " + result[i].coolness_points + "</p>";
+//       html += "<p>Attitude: " + result[i].attitude + "</p></li>";
+//     }
+//     html += "</ul>";
+//     res.send(html);
+//   });
